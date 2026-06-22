c5ca9892705037258d713f3043be1dae8749a3e1
dr1810 <93988901+dr1810@users.noreply.github.com>
2026-06-22 20:29:58 +0530

    ml component

---
+++ ml/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class EnergyPredictor(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(8, 64),
+            nn.ReLU(),
+
+            nn.Linear(64, 32),
+            nn.ReLU(),
+
+            nn.Linear(32, 2)
+        )
+
+    def forward(self, x):
+        return self.network(x)
